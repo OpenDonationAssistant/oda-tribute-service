c5fe888747f6c30afc86a412ef9c3397da37b5a5
stCarolas <stcarolas@gmail.com>
2026-07-31 00:20:56 +0300

    fix missing bindings

--- src/main/java/io/github/opendonationassistant/Application.java
+++ src/main/java/io/github/opendonationassistant/Application.java
@@ -1,10 +1,13 @@
 package io.github.opendonationassistant;
 
+import io.github.opendonationassistant.rabbit.AMQPConfiguration;
+import io.github.opendonationassistant.rabbit.Exchange;
 import io.github.opendonationassistant.rabbit.RabbitClient;
 import io.micronaut.context.ApplicationContextBuilder;
 import io.micronaut.context.ApplicationContextConfigurer;
 import io.micronaut.context.annotation.ContextConfigurer;
 import io.micronaut.context.annotation.Factory;
+import io.micronaut.rabbitmq.connect.ChannelInitializer;
 import io.micronaut.rabbitmq.connect.ChannelPool;
 import io.micronaut.runtime.Micronaut;
 import io.micronaut.serde.ObjectMapper;
@@ -12,6 +15,7 @@ import io.swagger.v3.oas.annotations.*;
 import io.swagger.v3.oas.annotations.info.*;
 import jakarta.inject.Named;
 import jakarta.inject.Singleton;
+import java.util.ArrayList;
 
 @OpenAPIDefinition(info = @Info(title = "oda-tribute-service"))
 @Factory
@@ -32,6 +36,13 @@ public class Application {
   }
 
   @Singleton
+  public ChannelInitializer rabbitConfiguration() {
+    var bindings = new ArrayList<Exchange>();
+    bindings.addAll(EventsListener.BINDING);
+    return new AMQPConfiguration(bindings);
+  }
+
+  @Singleton
   @Named("commands")
   public RabbitClient commandsFacade(ChannelPool pool, ObjectMapper mapper) {
     return new RabbitClient(pool, mapper, "commands");
@@ -43,4 +54,3 @@ public class Application {
     return new RabbitClient(pool, mapper, "tribute");
   }
 }
-
